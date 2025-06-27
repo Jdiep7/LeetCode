f9ea106cf57750f33ff6e57be2a0b1b8f8d39824
Jdiep7 <jdiep705@gmail.com>
2025-06-27 15:27:30 -0400

    Update

--- TrappingRainWater.py
+++ TrappingRainWater.py
@@ -1,4 +1,19 @@
 def trap(height):
+    left = 0
+    right = len(height) - 1
+    maxL, maxR = 0,0
+    result = 0
+    while left <= right:
+        if height[left] < height[right]: 
+            maxL = max(height[left], maxL)
+            result += maxL - height[left]
+            left += 1
+        else:
+            maxR = max(height[right], maxR)
+            result += maxR - height[right]
+            right -= 1
     
-    return
+    return result
+
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
         
