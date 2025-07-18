40966c444ec732f0fee5d035568999d9466cd7cb
Jdiep7 <jdiep705@gmail.com>
2025-07-18 10:59:01 -0400

    Update

--- GenerateParentheses.py
+++ GenerateParentheses.py
@@ -1,6 +1,15 @@
 from collections import deque
 
 def generateParenthesis(n):
-    start = "("
+    res = [[] for _ in range(n+1)]
+    res[0] = [""]
+    
+    for k in range(n + 1):
+        for i in range(k):
+            for left in res[i]:
+                for right in res[k-i-1]:
+                    res[k].append("(" + left + ")" + right)
+    
+    return res[-1]
     
     
