02231ef76ae6df4ed646c72a2dc4e7f63f183002
Jdiep7 <jdiep705@gmail.com>
2025-07-28 17:40:56 -0400

    Updates

---
+++ ReverseLinkedList.py
@@ -0,0 +1,24 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def reverseList(head):
+    currentNode, prev = head, None
+    while currentNode:
+        next_node = currentNode.next
+        currentNode.next = prev
+        prev = currentNode
+        currentNode = next_node
+        
+    
+    return head
+        
+        
+        
+        
+        
+
+
+
+
